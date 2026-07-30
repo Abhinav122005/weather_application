c62a2b43d74ad51c2a963c5a836c2918b1cd9e55
Abhinav Rayabarapu <rayabarapuabhinav@gmail.com>
2026-07-30 16:37:13 +0530

    initial commit

--- weather_application/settings.py
+++ weather_application/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
-environ.Env.read_csv(os.path.join(BASE_DIR,'.env'))
+environ.Env.read_env(os.path.join(BASE_DIR,'.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
