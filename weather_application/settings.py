f02c074bdb3b88ad4eb2857d7a1339f36acfc2d2
Abhinav Rayabarapu <rayabarapuabhinav@gmail.com>
2026-07-30 16:46:32 +0530

    Configure STATIC_ROOT and WhiteNoise for production

--- weather_application/settings.py
+++ weather_application/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,13 +126,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
-
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 SESSION_COOKIE_AGE = env('SESSION_COOKIE_AGE')   # 1 week
